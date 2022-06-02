7b7719bfab398479294167330a9c07b60da39482
Nishinoyama <begoingto1414@gmail.com>
2022-06-02 10:36:39 +0900

    add: traceability for the test, add more tests.

--- src/main/test/jp/ac/nara_k/info/verilog_netlist/parser/NetlistParsersTest.scala
+++ src/main/test/jp/ac/nara_k/info/verilog_netlist/parser/NetlistParsersTest.scala
@@ -23,26 +23,71 @@ class NetlistParsersTest extends AnyFunSuite with NetlistParsers {
     new NetlistTokenReader(lex(net))
   }
 
-  private def _test_parser[T](parser: Parser[T], net_excepted: (String, T)): Unit = {
-    assert(parser(genTokenReader(net_excepted._1)).get.equals(net_excepted._2))
+  private def _test_parser[T](parser: Parser[T], net_excepted: (String, T)): Boolean = {
+    val parsed = parser(genTokenReader(net_excepted._1))
+    if (parsed.successful) {
+      assert(parsed.get.equals(net_excepted._2), parsed.get + " != " + net_excepted._2)
+      true
+    } else {
+      false
+    }
   }
 
   private def test_parser[T] = (_test_parser[T] _).curried
 
-  test("NetlistParsers.identifier") {
+  private def test_parser_for_each[T](parser: Parser[T], net_excepted: (String, T)*): Unit = {
+    net_excepted.foreach(test_parser(parser))
+  }
+
+  test("NetlistParsers.Identifier") {
     test_parser(identifier)("m13.m141.n13", "m13.m141.n13")
     test_parser(indexed_identifier)("datai[12]", IndexedIdentifier("datai", 12))
   }
 
-  test("NetlistParsers.expression") {
-    List(
+  test("NetlistParsers.Expression") {
+    test_parser_for_each(expression,
       ("0", Number(0)),
       ("1", Number(1)),
       ("1'b0", Number(0)),
       ("1'b1", Number(1)),
       ("iden", SingleIdentifier("iden")),
       ("idx[3]", IndexedIdentifier("idx", 3)),
-    ).foreach(test_parser(expression))
+    )
+    test_parser_for_each(lvalue,
+      ("iden", SingleIdentifier("iden")),
+      ("idx[3]", IndexedIdentifier("idx", 3)),
+    )
+  }
+
+  test("NetlistParsers.BehavioralStatements") {
+    test_parser_for_each(range,
+      ("[0:20]", (20, 0)),
+      ("[16:32]", (32, 16)),
+    )
+    test_parser_for_each(assignment,
+      ("datao[3] = n35", (IndexedIdentifier("datao", 3), SingleIdentifier("n35"))),
+      ("test_so = 0", (SingleIdentifier("test_so"), Number(0))),
+      ("test_so = 1'b1", (SingleIdentifier("test_so"), Number(1))),
+    )
+  }
+
+  test("NetlistParsers.ModuleInstantiations") {
+    test_parser_for_each(namedPortConnection,
+      (".port_ident(wire_ident)", ("port_ident", SingleIdentifier("wire_ident"))),
+      (".D(n5[3])", ("D", IndexedIdentifier("n5", 3))),
+    )
+    test_parser_for_each(listOfModuleConnections,
+      (".A(n1), .B(n2), .Z(n3)", List(("A", SingleIdentifier("n1")), ("B", SingleIdentifier("n2")), ("Z", SingleIdentifier("n3")))),
+      (".D(n4), .DN(n5[3])", List(("D", SingleIdentifier("n4")), ("DN", IndexedIdentifier("n5", 3)))),
+    )
+    test_parser_for_each(nameOfInstance,
+      ("U1", SingleIdentifier("U1")),
+      ("U3 [12:2]", ArrayedIdentifier("U3", (2, 12))),
+    )
+    test_parser_for_each(moduleInstance,
+      ("U1 (.A(x))", (SingleIdentifier("U1"), List(("A", SingleIdentifier("x"))))),
+      ("U2 [3:0]()", (ArrayedIdentifier("U2", (0, 3)), List())),
+    )
   }
 
   test("NetlistParsers.parseISCAS_b02") {
